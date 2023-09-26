027ffc68f590bb69e699fd4e9d78ebf3f846fc06
Achmad Baihaqi <haqiachd@gmail.com>
2023-09-26 17:25:45 +0700

    update nav bottom

--- app/src/main/java/com/polije/sem3/menu/MainActivity.java
+++ app/src/main/java/com/polije/sem3/menu/MainActivity.java
@@ -5,56 +5,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
 import com.polije.sem3.R;
 import com.polije.sem3.util.FragmentUtil;
 
-import kotlin.Unit;
-import kotlin.jvm.functions.Function1;
 
 public class MainActivity extends AppCompatActivity {
 
-    MeowBottomNavigation bottomNavigation;
+    public static int MENU_HOME = 1, MENU_AKTIVITAS = 2, MENU_REFERENSI = 3, MENU_PROFILE = 4;
+
+    private MeowBottomNavigation bottomNavigation;
+    private TextView txtAppbar;
+
+    private void initViews(){
+        bottomNavigation = findViewById(R.id.bottomNav);
+        txtAppbar = findViewById(R.id.txt_appbar);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
 
-        bottomNavigation = findViewById(R.id.bottomNav);
-        bottomNavigation.add(new MeowBottomNavigation.Model(1, R.drawable.ic_bottom_nav_home));
-        bottomNavigation.add(new MeowBottomNavigation.Model(2, R.drawable.ic_bottom_nav_activity));
-        bottomNavigation.add(new MeowBottomNavigation.Model(3, R.drawable.ic_bottom_nav_referensi));
-        bottomNavigation.add(new MeowBottomNavigation.Model(4, R.drawable.ic_bottom_nav_profile));
+        bottomNavigation.add(new MeowBottomNavigation.Model(MENU_HOME, R.drawable.ic_bottom_nav_home));
+        bottomNavigation.add(new MeowBottomNavigation.Model(MENU_AKTIVITAS, R.drawable.ic_bottom_nav_activity));
+        bottomNavigation.add(new MeowBottomNavigation.Model(MENU_REFERENSI, R.drawable.ic_bottom_nav_referensi));
+        bottomNavigation.add(new MeowBottomNavigation.Model(MENU_PROFILE, R.drawable.ic_bottom_nav_profile));
 
-        bottomNavigation.setOnClickMenuListener(new Function1<MeowBottomNavigation.Model, Unit>() {
-            @Override
-            public Unit invoke(MeowBottomNavigation.Model model) {
-
-                switch (model.getId()){
-                    case 1 : {
-                        Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
-                    }
-                    case 2 : {
-                        Toast.makeText(MainActivity.this, "Activity", Toast.LENGTH_SHORT).show();
-                    }
-                    case 3 : {
-                        Toast.makeText(MainActivity.this, "Referensi", Toast.LENGTH_SHORT).show();
-                    }
-                    case 4 : {
-                        Toast.makeText(MainActivity.this, "Profile", Toast.LENGTH_SHORT).show();
-                    }
-
-                }
-
-                return null;
-            }
-        });
-
+        bottomNavigation.show(MENU_HOME, true);
         FragmentUtil.switchFragmentMain(getSupportFragmentManager(), new HomeFragment(), false);
+
+        onClickGroups();
     }
 
     @Override
@@ -76,4 +60,37 @@ public class MainActivity extends AppCompatActivity {
                 })
                 .create().show();
     }
+
+    private void onClickGroups(){
+
+        bottomNavigation.setOnClickMenuListener(model -> {
+
+            switch (model.getId()) {
+                case 1: {
+                    FragmentUtil.switchFragmentMain(MainActivity.this.getSupportFragmentManager(), new HomeFragment(), false);
+                    txtAppbar.setText(R.string.menu_home);
+                    break;
+                }
+                case 2: {
+                    FragmentUtil.switchFragmentMain(MainActivity.this.getSupportFragmentManager(), new AktivitasFragment(), false);
+                    txtAppbar.setText(R.string.menu_aktivitas);
+                    break;
+                }
+                case 3: {
+                    FragmentUtil.switchFragmentMain(MainActivity.this.getSupportFragmentManager(), new ReferensiFragment(), false);
+                    txtAppbar.setText(R.string.menu_referensi);
+                    break;
+                }
+                case 4: {
+                    FragmentUtil.switchFragmentMain(MainActivity.this.getSupportFragmentManager(), new ProfileFragment(), false);
+                    txtAppbar.setText(R.string.menu_profile);
+                    break;
+                }
+
+            }
+
+            return null;
+        });
+    }
+
 }
